16f28367578df935f9a540fbefc318481bc51bc5
nikos zevgolis <nzevgolisda@gmail.com>
2021-11-09 10:20:32 +0200

    Requested value print

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 
 //const ExcelJS = require('exceljs');
-var reader = require('any-text');
-var text = reader.getText(`./id.docx`);
-text.then(
-    function (data) {
-        console.log(data);
+let reader = require('any-text');
+let text = reader.getText(`./id.docx`);
+let obj = text.then(function (data) {
+    console.log(get(data));
+});
+
+function get(data) {
+    let dataPers = [];
+    let List = data.split(', ');
+    let i = 0;
+    while (i < List.length) {
+        let p = List[i].split(':');
+        let value = p[1];
+        dataPers.push(value);
+        i++;
     }
-);
+    return dataPers;
+}
